379acb4c155764381ff5cb23aba07a932d80a5ee
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-11-01 14:51:55 +0300

    added steps (and DisplayName)

--- src/test/java/tests/GetTests.java
+++ src/test/java/tests/GetTests.java
@@ -1,85 +1,110 @@
 package tests;
 
+import models.SingleUserNotFoundResponseModel;
 import models.SingleUserSuccessfulResponseModel;
 import models.UserListSuccessfulResponseModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
 
+@DisplayName("GET-запросы")
 public class GetTests extends TestBase {
 
+    @DisplayName("Проверка данных одиночного пользователя")
     @Test
     void successfulGetSingleUserTest() {
 
-        SingleUserSuccessfulResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-        .when()
-                .get("/users/2")
+        SingleUserSuccessfulResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+            .when()
+                    .get("/users/2")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(SingleUserSuccessfulResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(200)
+                    .extract().as(SingleUserSuccessfulResponseModel.class)
+        );
 
-        assertThat(response.getData().getId()).isEqualTo(2);
-        assertThat(response.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
-        assertThat(response.getData().getFirst_name()).isEqualTo("Janet");
-        assertThat(response.getData().getLast_name()).isEqualTo("Weaver");
-        assertThat(response.getData().getAvatar()).isEqualTo("https://reqres.in/img/faces/2-image.jpg");
+        step("Проверить ответ", ()-> {
+            assertThat(response.getData().getId()).isEqualTo(2);
+            assertThat(response.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
+            assertThat(response.getData().getFirst_name()).isEqualTo("Janet");
+            assertThat(response.getData().getLast_name()).isEqualTo("Weaver");
+            assertThat(response.getData().getAvatar()).isEqualTo("https://reqres.in/img/faces/2-image.jpg");
 
-        assertThat(response.getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
-        assertThat(response.getSupport().getText()).isEqualTo("To keep ReqRes free, contributions towards server costs are appreciated!");
+            assertThat(response.getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
+            assertThat(response.getSupport().getText()).isEqualTo("To keep ReqRes free, contributions towards server costs are appreciated!");
+        });
     }
 
+    @DisplayName("Проверка данных, когда пользователь не найден")
     @Test
     void singleUserNotFoundTest() {
 
-        given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-        .when()
-                .get("/users/23")
+        SingleUserNotFoundResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+            .when()
+                    .get("/users/23")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(404);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(404)
+                    .extract().as(SingleUserNotFoundResponseModel.class)
+        );
+
+        step("Проверить ответ", ()->
+            assertThat(response).isNotNull()
+        );
     }
 
+    @DisplayName("Проверка данных списка всех пользователей")
     @Test
     void getUsersListTest() {
 
-        UserListSuccessfulResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-        .when()
-                .get("/users?page=2")
+        UserListSuccessfulResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+            .when()
+                    .get("/users?page=2")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(UserListSuccessfulResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(200)
+                    .extract().as(UserListSuccessfulResponseModel.class)
+        );
 
-        assertThat(response.getPage()).isEqualTo(2);
-        assertThat(response.getPer_page()).isEqualTo(6);
-        assertThat(response.getTotal()).isEqualTo(12);
-        assertThat(response.getTotal_pages()).isEqualTo(2);
+        step("Проверить ответ", ()-> {
+            assertThat(response.getPage()).isEqualTo(2);
+            assertThat(response.getPer_page()).isEqualTo(6);
+            assertThat(response.getTotal()).isEqualTo(12);
+            assertThat(response.getTotal_pages()).isEqualTo(2);
 
-        assertThat(response.getData().get(3).getId()).isEqualTo(10);
-        assertThat(response.getData().get(3).getEmail()).isEqualTo("byron.fields@reqres.in");
-        assertThat(response.getData().get(3).getFirst_name()).isEqualTo("Byron");
-        assertThat(response.getData().get(3).getLast_name()).isEqualTo("Fields");
-        assertThat(response.getData().get(3).getAvatar()).isEqualTo("https://reqres.in/img/faces/10-image.jpg");
+            assertThat(response.getData().get(3).getId()).isEqualTo(10);
+            assertThat(response.getData().get(3).getEmail()).isEqualTo("byron.fields@reqres.in");
+            assertThat(response.getData().get(3).getFirst_name()).isEqualTo("Byron");
+            assertThat(response.getData().get(3).getLast_name()).isEqualTo("Fields");
+            assertThat(response.getData().get(3).getAvatar()).isEqualTo("https://reqres.in/img/faces/10-image.jpg");
 
-        assertThat(response.getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
-        assertThat(response.getSupport().getText()).isEqualTo("To keep ReqRes free, contributions towards server costs are appreciated!");
+            assertThat(response.getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
+            assertThat(response.getSupport().getText()).isEqualTo("To keep ReqRes free, contributions towards server costs are appreciated!");
+        });
     }
 }
--- src/test/java/tests/PostTests.java
+++ src/test/java/tests/PostTests.java
@@ -1,14 +1,19 @@
 package tests;
 
 import models.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("POST-запросы")
 public class PostTests extends TestBase {
 
+    @DisplayName("Проверка возвращаемых данных при создании нового пользователя")
     @Test
     void createUserTest() {
 
@@ -16,28 +21,34 @@ public class PostTests extends TestBase {
         request.setName("alex");
         request.setJob("qa engineer");
 
-        CreateUserSuccessfulResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-                .body(request)
-                .contentType(JSON)
+        CreateUserSuccessfulResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+                    .body(request)
+                    .contentType(JSON)
 
-        .when()
-                .post("/users")
+            .when()
+                    .post("/users")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(201)
-                .extract().as(CreateUserSuccessfulResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(201)
+                    .extract().as(CreateUserSuccessfulResponseModel.class)
+        );
 
-        assertThat(response.getName()).isEqualTo(request.getName());
-        assertThat(response.getJob()).isEqualTo(request.getJob());
-        assertThat(response.getId()).isNotEmpty();
-        assertThat(response.getCreatedAt()).isNotEmpty();
+        step("Проверить ответ", ()-> {
+            assertThat(response.getName()).isEqualTo(request.getName());
+            assertThat(response.getJob()).isEqualTo(request.getJob());
+            assertThat(response.getId()).isNotEmpty();
+            assertThat(response.getCreatedAt()).isNotEmpty();
+        });
     }
 
+    @DisplayName("Проверка возвращаемых данных при регистрации нового пользователя")
     @Test
     void successfulRegistrationTest() {
 
@@ -45,73 +56,90 @@ public class PostTests extends TestBase {
         request.setEmail("eve.holt@reqres.in");
         request.setPassword("pistol");
 
-        RegistrationSuccessfulResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-                .body(request)
-                .contentType(JSON)
+        RegistrationSuccessfulResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+                    .body(request)
+                    .contentType(JSON)
 
-        .when()
-                .post("/register")
+            .when()
+                    .post("/register")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(RegistrationSuccessfulResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(200)
+                    .extract().as(RegistrationSuccessfulResponseModel.class)
+        );
 
-        assertThat(response.getId()).isNotEqualTo(0);
-        assertThat(response.getToken()).isNotEmpty();
+        step("Проверить ответ", ()-> {
+            assertThat(response.getId()).isNotEqualTo(0);
+            assertThat(response.getToken()).isNotEmpty();
+        });
     }
 
+    @DisplayName("Проверка возвращаемых данных при не корректной регистрации нового пользователя (регистрация без пароля)")
     @Test
     void unsuccessfulRegistrationMissingPasswordTest() {
 
         RegistrationRequestModel request = new RegistrationRequestModel();
         request.setEmail("eve.holt@reqres.in");
 
-        RegistrationMissingEmailOrPasswordResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-                .body(request)
-                .contentType(JSON)
+        RegistrationMissingEmailOrPasswordResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+                    .body(request)
+                    .contentType(JSON)
 
-        .when()
-                .post("/register")
+            .when()
+                    .post("/register")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .extract().as(RegistrationMissingEmailOrPasswordResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(400)
+                    .extract().as(RegistrationMissingEmailOrPasswordResponseModel.class)
+        );
 
-        assertThat(response.getError()).isEqualTo("Missing password");
+        step("Проверить ответ", ()->
+            assertThat(response.getError()).isEqualTo("Missing password")
+        );
     }
 
+    @DisplayName("Проверка возвращаемых данных при не корректной регистрации нового пользователя (регистрация без емейла)")
     @Test
     void unsuccessfulRegistrationMissingEmailTest() {
 
         RegistrationRequestModel request = new RegistrationRequestModel();
         request.setPassword("pistol");
 
-        RegistrationMissingEmailOrPasswordResponseModel response = given()
-                .log().uri()
-                .log().body()
-                .log().headers()
-                .body(request)
-                .contentType(JSON)
+        RegistrationMissingEmailOrPasswordResponseModel response = step("Сделать запрос", ()->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+                    .body(request)
+                    .contentType(JSON)
 
-        .when()
-                .post("/register")
+            .when()
+                    .post("/register")
 
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .extract().as(RegistrationMissingEmailOrPasswordResponseModel.class);
+            .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(400)
+                    .extract().as(RegistrationMissingEmailOrPasswordResponseModel.class)
+        );
 
-        assertThat(response.getError()).isEqualTo("Missing email or username");
+        step("Проверить ответ", ()->
+            assertThat(response.getError()).isEqualTo("Missing email or username")
+        );
     }
 }
