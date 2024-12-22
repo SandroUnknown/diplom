715499d3a038a6cf0ecaf316c22125aabb3817fe
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-22 19:22:53 +0300

    Create LabelResponseModel.java

---
+++ src/test/java/models/labels/LabelResponseModel.java
@@ -0,0 +1,19 @@
+package models.labels;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LabelResponseModel {
+
+    private String id;
+    private String name;
+    @JsonProperty("new_name") private String newName;
+    private Color color;
+    private int order;
+    @JsonProperty("is_favoriteg") private boolean isFavorite;
+}
