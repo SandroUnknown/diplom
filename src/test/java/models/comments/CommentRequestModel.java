1116c1ad0e357f32132918bca3987bcc09d059a5
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-22 20:16:47 +0300

    Create CommentRequestModel.java

---
+++ src/test/java/models/comments/CommentRequestModel.java
@@ -0,0 +1,26 @@
+package models.comments;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommentRequestModel {
+
+    @JsonProperty("task_id") private String taskId;
+    @JsonProperty("project_id") private String projectId;
+    private String content;
+    private Attachment attachment;
+
+    private static class Attachment {
+        @JsonProperty("resource_type") private String resourceType;
+        @JsonProperty("file_url") private String fileUrl;
+        @JsonProperty("file_type") private String fileType;
+        @JsonProperty("file_name") private String fileName;
+    }
+}
