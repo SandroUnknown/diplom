d31c8e508b1c19ed96069d59ff245c693aaff41e
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-22 19:18:50 +0300

    Create LabelRequestModel.java

---
+++ src/test/java/models/labels/LabelRequestModel.java
@@ -0,0 +1,20 @@
+package models.labels;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LabelRequestModel {
+
+    private String name;
+    @JsonProperty("new_name") private String newName;
+    private Color color;
+    private int order;
+    @JsonProperty("is_favoriteg") private boolean isFavorite;
+}
