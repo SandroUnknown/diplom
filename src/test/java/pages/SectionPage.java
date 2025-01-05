09e8a547964e983117e8e15f814b669714ad9261
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2025-01-05 13:28:19 +0300

    Update SectionPage.java

--- src/test/java/pages/SectionPage.java
+++ src/test/java/pages/SectionPage.java
@@ -27,8 +27,14 @@ public class SectionPage {
             sectionNameInputElement = $("div.board_view__add_section input"),
             createSectionButtonElement = $("div.board_view__add_section button[type='submit']"),
             deleteSectionButtonElement = $$("div.reactist_menulist div").last(),
-            confirmDeleteSectionButtonElement = $$("div[data-testid='modal-overlay'] footer button").last();
+            confirmDeleteSectionButtonElement = $$("div[data-testid='modal-overlay'] footer button").last(),
+            separatorBetweenSectionsElement = $$("div.board_add_section_trigger__container");   // он не СелинидЭлемент !
 
+    private final SelenideElement
+            sectionCheckElement = $$("[data-testid='board-section']"),    // он не СелинидЭлемент !
+    
+            addProjectButtonElement = $("[aria-label='Добавить проект']"),
+            projectNameInputElement = $("input[name='name']"),
     
 
     /*private final SelenideElement
@@ -65,6 +71,13 @@ public class SectionPage {
         addSectionButtonElement.click();
         return this;
     }
+
+    @Step("Навести мышку на разделительную линию между разделами и нажать на неё.")
+    public SectionPage clickOnSeparatorBetweenSections(int separatorIndex) {
+        //separatorBetweenSectionsElement.get(separatorIndex).hover(); // TODO : надо ли на него наводиться мышкой?
+        separatorBetweenSectionsElement.get(separatorIndex).click();
+        return this;
+    }
     
     @Step("Ввести имя раздела.")
     public SectionPage inputSectionName(String sectionName) {
@@ -93,6 +106,20 @@ public class SectionPage {
     @Step("Подвердить удаление раздела.")
     public SectionPage clickConfirmDeleteSectionButtonElement() {
         confirmDeleteSectionButtonElement.click();
+        return this;
+    }
+
+    // === ПРОВЕРКИ ================================
+
+    @Step("Проверить, что раздел был успешно создан в указанном месте.")
+    public SectionPage checkSuccsessfulCreatedSection(int sectionIndex, String sectionName) {
+        sectionCheckElement.get(sectionIndex).shouldHave(text(sectionName));
+        return this;
+    }
+
+    @Step("Проверить, что раздел был успешно удалён (отсутствует в списке разделов).")
+    public SectionPage checkSuccsessfulDeleteSection(String sectionName) {
+        sectionCheckElement.forEach(element -> element.shouldNotHave(text(sectionName)));
         return this;
     }
 
