6b98832a45e725b6dadf33365f1ba90b26f1e88b
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2025-01-08 17:02:20 +0300

    Create ProjectField.java

---
+++ src/test/java/enums/ProjectField.java
@@ -0,0 +1,13 @@
+package enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+
+@Getter
+public enum ProjectField {
+    NAME,
+    COLOR,
+    FAVORITE,
+    VIEW_STYLE
+}
