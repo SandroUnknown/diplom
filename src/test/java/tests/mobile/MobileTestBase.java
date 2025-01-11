3190f3dff4d458732e84add7a04f6fb52888b7e9
Alexander Petyukov <sandrounknown1991@gmail.com>
2025-01-11 15:32:28 +0300

    init commit diploma

--- src/test/java/tests/mobile/MobileTestBase.java
+++ src/test/java/tests/mobile/MobileTestBase.java
@@ -3,7 +3,7 @@ package tests.mobile;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.BrowserstackDriver;
-import drivers.EmulationDriver;
+//import drivers.EmulationDriver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
