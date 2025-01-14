f019386d5dc06e2e4922c090239085345f106cbc
Alexander Petyukov <sandrounknown1991@gmail.com>
2025-01-14 12:09:11 +0300

    init commit diploma

--- src/test/java/screens/components/PopupScreen.java
+++ src/test/java/screens/components/PopupScreen.java
@@ -7,11 +7,11 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class PopupScreen {
 
-    /*private final SelenideElement popupYesButtonElement = $(By.xpath(
-            "//android.widget.Button[@resource-id='android:id/button3']"));*/
+    private final SelenideElement popupYesButtonElement = $(By.xpath(
+            //"//android.widget.Button[@resource-id='android:id/button3']"));
+            "//android.widget.Button[@text='NEVER ASK']"));
 
-    private final SelenideElement popupYesButtonElement = $(
-            "android:id/button3");
+    //private final SelenideElement popupYesButtonElement = $("android:id/button3");
 
     public PopupScreen clickYesButton() {
         if (popupYesButtonElement.isDisplayed()) {
