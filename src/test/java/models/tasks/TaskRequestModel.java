03fc45a218d42a7f1675e85c5e02ddd7f3533b70
Alexander Petyukov <sandrounknown1991@gmail.com>
2025-01-14 23:15:53 +0300

    init commit diploma

--- src/test/java/models/tasks/TaskRequestModel.java
+++ src/test/java/models/tasks/TaskRequestModel.java
@@ -21,12 +21,4 @@ public class TaskRequestModel {
     @JsonProperty("section_id") private String sectionId;
     @JsonProperty("parent_id") private String parentId;
     @JsonProperty("assignee_id") private String assigneeId;
-
-    /*private String duration;
-    @JsonProperty("duration_unit") private String durationUnit;
-
-    @JsonProperty("due_string") private String dueString;
-    @JsonProperty("due_date") private String dueDate;
-    @JsonProperty("due_datetime") private String dueDatetime;
-    @JsonProperty("due_lang") private String dueLang;*/
 }
