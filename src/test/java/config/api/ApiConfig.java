7337863ba5ae8d92ae8e3eb34c0be7013e8e4cc0
Alexander Petyukov <sandrounknown1991@gmail.com>
2025-01-15 11:56:45 +0300

    init commit diploma

--- src/test/java/config/api/ApiConfig.java
+++ src/test/java/config/api/ApiConfig.java
@@ -4,6 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources({
         "classpath:properties/${env}.properties",
+        "classpath:properties/${apiEnv}.properties",
         "classpath:properties/local.properties"
 })
 public interface ApiConfig extends Config {
--- src/test/java/config/web/WebConfig.java
+++ src/test/java/config/web/WebConfig.java
@@ -4,6 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources({
         "classpath:properties/${env}.properties",
+        "classpath:properties/${webEnv}.properties",
         "classpath:properties/local.properties"
 })
 public interface WebConfig extends Config {
